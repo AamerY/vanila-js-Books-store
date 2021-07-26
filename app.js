aa9a559e269e2d24e72d9d41b42064c930dd5488
AamerY <negmaa@gmail.com>
2021-07-26 13:44:36 +0300

    addbook

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const booksList = document.querySelector(".books-list");
 
 
 //Event Listeners
-document.addEventListener("DOMContentLoaded", getBooks);
 booksButton.addEventListener("click", addBook);
 booksList.addEventListener("click", deleteBook);
 booksList.addEventListener("click", editBook);
@@ -27,12 +26,28 @@ function addBook(e) {
     //Prevent natural behaviour
     e.preventDefault();
     //Create books div
-    const booksDiv = document.createElement("div");
+    const booksDiv = document.createElement("div")
     booksDiv.classList.add("books");
     //Create list
     const newBook = document.createElement("li");
     newBook.innerText = booksInput.value;
   
+    //
+    newBook.classList.add("books-item");
+    booksDiv.appendChild(newBook);
+    booksInput.value = "";
+    //Create Edited Button
+    const editdButton = document.createElement("button");
+    editdButton.innerHTML = `<i class="fas fa-edit"></i>`;
+    editdButton.classList.add("edit-btn");
+    booksDiv.appendChild(editdButton);
+    //Create trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    trashButton.classList.add("trash-btn");
+    booksDiv.appendChild(trashButton);
+    //attach final Book
+    booksList.appendChild(booksDiv);
   }
   
   function deleteBook(e) {
