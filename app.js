68ae21c797f9f45de9c1114a09b893bc5f9e61a7
AamerY <negmaa@gmail.com>
2021-07-27 15:29:49 +0300

    ver3

--- app.js
+++ app.js
@@ -1,78 +1,88 @@
-setInterval(myclock, 1000);
+const container = document.querySelector('.container');
+var inputValue = document.querySelector('.input');
+const add = document.querySelector('.add-btn');
 
-function myclock() {
-  let d = new Date();
-  document.getElementById("clock").innerHTML =
-    d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+if(window.localStorage.getItem("todos") == undefined){
+     var todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-//Select DOM
-const booksInput = document.querySelector(".books-input");
-const booksButton = document.querySelector(".books-button");
-const booksList = document.querySelector(".books-list");
+var todosEX = window.localStorage.getItem("todos");
+var todos = JSON.parse(todosEX);
 
-//Event Listeners
-booksButton.addEventListener("click", addBook);
-booksList.addEventListener("click", handleClick);
-//booksList.baddEventListener("click", editBook);
-//booksList.addEventListener("click", function() {
-//   addBook(val);
-// });
 
-//Functions
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	var itemBox = document.createElement('div');
+        itemBox.classList.add('item');
 
-function addBook(e) {
-  //Prevent natural behaviour
-  // e.preventDefault();
-  //Create books div
-  const booksDiv = document.createElement("div");
-  booksDiv.classList.add("books");
-  //Create list
-  const newBook = document.createElement("li");
-  newBook.innerText = booksInput.value;
+    	var input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
 
-  //
-  newBook.classList.add("books-item");
-  booksDiv.appendChild(newBook);
-  booksInput.value = "";
-  //Create Edited Button
-  const editdButton = document.createElement("button");
-  editdButton.innerHTML = `<i class="fas fa-edit"></i>`;
-  editdButton.classList.add("edit-btn");
-  booksDiv.appendChild(editdButton);
-  //Create trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  trashButton.classList.add("trash-btn");
-  booksDiv.appendChild(trashButton);
-  //attach final Book
-  booksList.appendChild(booksDiv);
+    	var edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = `<i class="fas fa-edit"></i>`;
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	var remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = `<i class="fas fa-trash"></i>`;
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
+
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
+
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
 }
 
-function handleClick(e) {
-  const item = e.target;
-  const books = item.parentElement;
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
 
-  if (item.classList[0] === "trash-btn") {
-    const books = item.parentElement;
-
-    books.remove();
-  }
-
-  if (item.classList[0] === "edit-btn") {
-    
-   //var input = books.innerText;
-
-    var input = document.createElement('input');
-    input.type = "text";
-    input.disabled = true;
-    
-
-    input.classList.add('books-input');
-
-    
-
-    console.log(item);
-    console.log(books);
-  }
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
 }
+
+
+for (var v = 0 ; v < todos.length ; v++){
+    new item(todos[v]);
+}
+
+
+new item("sport");
